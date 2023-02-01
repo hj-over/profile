7ee7dc8e0f8e0bdd315eee6a8d537a619a6b0c1f
hj.over <dhfldhs0707@gmail.com>
2023-02-01 14:22:48 +0900

    useEffect 및 useRef 를 활용한 모션 작업

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => {
-  const active = { color: "blue" };
+  const active = { color: "pink" };
   return (
     <header className={props.type}>
       <div className="inner">
--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../common/Layout";
-
+// jascript 코드 자리
 const Community = () => {
+  // jascript 코드 자리
+  // 컴포넌트가 마운트, 업데이트(state 변경), 언마운트 시
+  // 하고 싶은일을 작성하는 Hook : useEffect
+  // 기본적 형태
+  // useEffect(() => {
+  //   하고 싶은일
+  //   클린업 함수 : 컴포넌트 제거시 실행
+  //   return () => {
+  //   }
+  // }, [의존성배열: dependency Array ]);
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  // jsx 코드 자리
+  // : 주의 사항 return () O
+  // : 주의 사항 return {} X
   return <Layout title={"Community"}>Community</Layout>;
 };
-
+// jascript 코드 자리
 export default Community;
