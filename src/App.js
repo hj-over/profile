ed842b2191163fb02d128a882eca2fd3f9b5f4fc
hj.over <dhfldhs0707@gmail.com>
2023-02-16 10:12:22 +0900

    카카오 로그인 / 로그아웃 적용

--- src/App.js
+++ src/App.js
@@ -14,8 +14,22 @@ import Youtube from "./components/sub/Youtube";
 import Location from "./components/sub/Location";
 import Join from "./components/sub/Join";
 import Schedule from "./components/sub/Schedule";
+import Login from "./components/sub/Login";
+import Logout from "./components/sub/Logout";
 
 function App() {
+  // 카카오 로그인 기능
+  // 등록된 앱의 JavaScript key
+  const jsKey = process.env.REACT_APP_KAKAO;
+
+  // SDK는 한 번만 초기화해야 한다.
+  // 중복되는 초기화를 막기 위해 isInitialized()로 SDK 초기화 여부를 판단한다.
+  if (!window.Kakao.isInitialized()) {
+    // JavaScript key를 인자로 주고 SDK 초기화
+    window.Kakao.init(jsKey);
+    // SDK 초기화 여부를 확인하자.
+    console.log(window.Kakao.isInitialized());
+  }
   return (
     <>
       {/* 화면에 중첩되는 컴포넌트가 있는 경우 Switch 를 활용한다. */}
@@ -41,7 +55,8 @@ function App() {
       <Route path="/youtube" component={Youtube} />
       <Route path="/location" component={Location} />
       <Route path="/join" component={Join} />
-
+      <Route path="/login" component={Login} />
+      <Route path="/login" component={Logout} />
       <Footer />
     </>
   );
---
+++ src/components/sub/Logout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const Logout = () => {
+  // 화면이동
+  const history = useHistory();
+  // 카카오 기능
+  // 카카오 로그아웃
+  const kakaoLogOut = () => {
+    if (!window.Kakao.Auth.getAccessToken()) {
+      console.log("Not logged in.");
+      return;
+    }
+    window.Kakao.Auth.logout(function (response) {
+      alert(response + " logout");
+      // window.location.href='/'
+      history.push("/");
+    });
+  };
+  // 카카오 서비스 탈퇴
+  const memberOut = () => {
+    window.Kakao.API.request({
+      url: "/v1/user/unlink",
+      success: function (response) {
+        console.log(response);
+        //callback(); //연결끊기(탈퇴)성공시 서버에서 처리할 함수
+        // window.location.href='/'
+        history.push("/");
+      },
+      fail: function (error) {
+        console.log("탈퇴 미완료");
+        console.log(error);
+      },
+    });
+  };
+  return (
+    <div>
+      <button onClick={kakaoLogOut}>카카오 로그아웃</button>
+      <button onClick={memberOut}>카카오 서비스 탈퇴</button>
+    </div>
+  );
+};
+
+export default Logout;
